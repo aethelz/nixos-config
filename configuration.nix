cd7857d3303e1bb7f08211aa7c5bb1262c61553a
Eugene <aethelz@users.noreply.github.com>
2019-04-26 21:41:44 +0300

    Acer update

--- configuration.nix
+++ configuration.nix
@@ -6,53 +6,68 @@
       ./hardware-configuration.nix
       "${builtins.fetchGit {
         url = "https://github.com/rycee/home-manager";
+        rev = "ff602cb906e3dd5d5f89c7c1d0fae65bc67119a0";
+        ref = "release-19.03";
       }}/nixos"
     ];
 
-  # Use the GRUB 2 boot loader.
-  boot.loader.grub.enable = true;
-  boot.loader.grub.version = 2;
-  # Define on which hard drive you want to install Grub.
-  boot.loader.grub.device = "/dev/sda";
+  boot = {
+    tmpOnTmpfs = true;
+    loader = {
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
+    };
+    kernelPackages = pkgs.linuxPackages_5_0;
+    resumeDevice = "/dev/nvme0n1p2";
+    kernelParams = [
+      "resume_offset=83968" # swap file offset
+      "i915.enable_fbc=1"
+      "i915.fastboot=1"
+      ];
+    kernel.sysctl = { "vm.swappiness" = 1; };
+  };
 
-  networking.hostName = "nixos"; # Define your hostname.
+  networking.hostName = "acer";
+  networking.networkmanager.enable = true;
 
-  # Select internationalisation properties.
-  # i18n = {
-  #   consoleFont = "Lat2-Terminus16";
-  #   consoleKeyMap = "us";
-  #   defaultLocale = "en_US.UTF-8";
-  # };
-
-  # Set your time zone.
   time.timeZone = "Europe/Moscow";
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
   environment.systemPackages = with pkgs; [
     bat
     exa
     fd
+    feh
     git
     gotop
     htop
+    libreoffice
+    moc
+    mpv
     ncdu
     neofetch
+    nmap
+    nfs-utils
+    pass
     ranger
-    ripgrep
-    silver-searcher
+    sshfs
     termite
     tig
-    vim
+    unzip
+    w3m
     wget
+    xcalib
+    xcape
+    xorg.xmodmap
+    xorg.xsetroot
+    youtube-dl
   ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.gnupg.agent = { enable = true; enableSSHSupport = true; };
+  fonts.fonts = with pkgs; [
+    terminus_font
+  ];
 
-  programs.bash.enableCompletion = true;
-  programs.tmux.enable = true;
+  programs.vim.defaultEditor = true;
+
   home-manager.users.gene = {
     home.file.".config/nvim/init.vim".source = "${builtins.fetchGit {
       url = "https://github.com/aethelz/dotfiles/";
@@ -63,58 +78,135 @@
     home.file.".config/ranger/rc.conf".source = "${builtins.fetchGit {
       url = "https://github.com/aethelz/dotfiles/";
     }}/ranger/.config/ranger/rc.conf";
+    home.file.".config/termite/config".source = "${builtins.fetchGit {
+      url = "https://github.com/aethelz/dotfiles/";
+    }}/termite/.config/termite/config";
+    home.file.".Xmodmap".source = "${builtins.fetchGit {
+      url = "https://github.com/aethelz/dotfiles/";
+    }}/xmodmap/.Xmodmap";
+    home.file.".config/acer.icm".source = "${builtins.fetchGit {
+      url = "https://github.com/aethelz/dotfiles/";
+    }}/color/acer.icm";
+    home.file.".config/i3/config".source = "${builtins.fetchGit {
+      url = "https://github.com/aethelz/dotfiles/";
+    }}/i3/.config/i3/config";
+    home.file.".inputrc".source = "${builtins.fetchGit {
+      url = "https://github.com/aethelz/dotfiles/";
+    }}/readline/.inputrc";
     home.packages = [ pkgs.atool pkgs.httpie ];
-    programs.fzf.enable = true;
-    programs.neovim.enable = true;
-    programs.neovim.vimAlias = true;
-    programs.neovim.viAlias = true;
-    programs.bash.enable = true;
-    programs.bash.sessionVariables = {
-      EDITOR = "nvim";
-      FZF_DEFAULT_COMMAND = "rg --files";
-      PS1 = ''\[\e[0;30m\e[44m\]\u@\h:\w\$\[\e[0m\] '';
-    };
-    programs.bash.shellAliases = {
-      dir = "exa --long --git --all";
-    };
-    programs.bash.shellOptions = [
-    ];
-    programs.bash.bashrcExtra = ''
-      rg() {
-        if [ -z "$RANGER_LEVEL" ]
-        then
+    programs = {
+      zathura.enable = true;
+      firefox.enable = true;
+      fzf.enable = true;
+      fzf.defaultCommand = "rg --files";
+      neovim.enable = true;
+      neovim.vimAlias = true;
+      neovim.viAlias = true;
+      bash = {
+        enable = true;
+        shellAliases = {
+          dir = "exa --long --git --all";
+        };
+        shellOptions = [ "autocd" ];
+        historyControl = [
+          "erasedups"
+          "ignoredups"
+        ];
+        historyIgnore = [ "cd" ];
+        bashrcExtra = ''
+          export EDITOR="nvim"
+          export PS1="\[\e[0;30m\e[44m\]\u@\h:\w\$\[\e[0m\] "
+          rg() {
+          if [ -z "$RANGER_LEVEL" ]
+          then
           ranger
-        else
+          else
           exit
-        fi
-      }
-      [ -n "$RANGER_LEVEL" ] && PS1="$PS1"'(in ranger) '
-      bind '"\e[5~": history-search-backward'
-      bind '"\e[6~": history-search-forward'
-      '';
+          fi
+          }
+          [ -n "$RANGER_LEVEL" ] && PS1="$PS1"'(in ranger) '
+        '';
+      };
+    };
   };
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = { enable = true; enableSSHSupport = true; };
 
   # List services that you want to enable:
 
-  # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
-
-  virtualisation.vmware.guest.enable = true;
-  virtualisation.vmware.guest.headless = true;
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.gene = {
-    isNormalUser = true;
-    uid = 1000;
-    home = "/home/gene";
-    extraGroups = [ "wheel" ];
+  # Enable sound.
+  sound.enable = true;
+  sound.mediaKeys.enable = true;
+
+  hardware = {
+    brightnessctl.enable = true;
+    opengl.enable = true;
+    pulseaudio.enable = true;
+    cpu.intel.updateMicrocode = true;
   };
 
-  system.stateVersion = "18.03"; # Did you read the comment?
+  services.tlp.enable = true;
+  services.journald.extraConfig = "SystemMaxUse=50M";
+
+  # Enable the X11 windowing system.
+  services.xserver = {
+    enable = true;
+    libinput.enable = true;
+    libinput.accelProfile = "flat";
+    layout = "us,ru";
+    # Change layout on left control
+    xkbOptions = "grp:lctrl_toggle";
+    videoDrivers = [ "intel" ];
+    # deviceSection = ''
+    #   Option "TearFree" "true"
+    # '';
+
+    desktopManager = {
+      default = "none";
+      xterm.enable = false;
+    };
+
+    displayManager.lightdm.greeters.mini = {
+      enable = true;
+      user = "gene";
+      extraConfig = ''
+      [greeter]
+      show-password-label = false
+      '';
+    };
+
+    windowManager = {
+      default = "i3";
+      i3 = {
+        enable = true;
+        extraPackages = with pkgs; [
+          dmenu
+          i3status
+          i3lock
+        ];
+        # swap ctrl and caps, apply color profile
+        extraSessionCommands = ''
+        xmodmap /home/gene/.Xmodmap && xcape -e 'Control_L=Escape'
+        xcalib -d :0 /home/gene/.config/acer.icm
+        '';
+      };
+    };
+  };
+
+  users.users.gene = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "networkmanager" "video" ];
+  };
+
+  system.stateVersion = "19.03";
 
 }
